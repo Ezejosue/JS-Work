7e5cc940ea0e22ad50063674129ecb4846936e33
Ezejosue <aezequiel56@gmail.com>
2021-09-30 15:51:55 -0600

    :sparkles: Uso de cupones.
    Por medio de un array con diferentes opciones de cupones se calcula el decuento dependiendo de cual ingrese el usuario.

--- js/descuentos.js
+++ js/descuentos.js
@@ -1,6 +1,6 @@
 function calcularPrecioDescuento(precio, descuento) {
-  const porcentajeprecio = 100 - descuento;
-  const precioDescuento = (precio * porcentajeprecio) / 100;
+  const porcentajeprecio = 100 - parseFloat(descuento);
+  const precioDescuento = (parseFloat(precio) * porcentajeprecio) / 100;
   return precioDescuento;
 }
 
@@ -14,4 +14,41 @@ function oncalcularPrecioDescuento() {
   const pResultado = document.getElementById("pResultado");
 
   pResultado.innerText = "El precio con descuento es: $" + precioDescuento;
+}
+
+const coupons = [
+  {
+    name: "Basic",
+    discount: 15,
+  },
+  {
+    name: "Medium",
+    discount: 30,
+  },
+  {
+    name: "Premium",
+    discount: 25,
+  },
+];
+
+function oncalcularPrecioDescuentoC() {
+  const input = document.getElementById("precio");
+  const precio = input.value;
+  const cupon = document.getElementById("cupon");
+  const cuponValor = cupon.value;
+
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === cuponValor;
+  };
+
+  const userCoupon = coupons.find(isCouponValueValid);
+
+  if (!userCoupon) {
+    alert("El cupón " + cuponValor + "no es válido");
+  } else {
+    const descuento = userCoupon.discount;
+    const precioConDescuento = calcularPrecioDescuento(precio, descuento);
+    const resultP = document.getElementById("pResultado");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
 }
